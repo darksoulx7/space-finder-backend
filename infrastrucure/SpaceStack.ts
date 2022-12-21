a21146c654a35c32b782f10a859c44abdeb2ea41
vishvas.prajapati <vishvas.prajapati@volansys.com>
2022-12-21 11:03:39 +0530

    lambda and dynamo table

---
+++ infrastrucure/SpaceStack.ts
@@ -0,0 +1,33 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { LambdaIntegration, RestApi } from "aws-cdk-lib/aws-apigateway";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { join } from "path";
+import { GenericTable } from "./GenericTable";
+
+export class SpaceStack extends Stack {
+  private api =new RestApi(this,'SpaceApi')
+  private spacesTable = new GenericTable(
+    'SpaceTable',
+    'spaceId',
+    this
+  )
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    const helloLambda = new LambdaFunction(this, "helloLambda", {
+      runtime: Runtime.NODEJS_16_X,
+      code: Code.fromAsset(join(__dirname,'..','services','hello')),
+      handler: 'hello.main'
+    });
+
+    // Hello Api lambda integreation
+    const helloLambdaIntegreation = new LambdaIntegration(helloLambda);
+    const helloLambdaResource = this.api.root.addResource('hello')
+    helloLambdaResource.addMethod('GET', helloLambdaIntegreation);
+  }
+}
---
+++ services/hello/hello.js
@@ -0,0 +1,6 @@
+exports.main = async function(Event,context) {
+    return {
+        statusCode:200,
+        body: 'Hello Lambda!'
+    }
+}
