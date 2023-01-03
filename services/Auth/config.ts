90832bfe1b15d7cc58169c457ca6dd37b5576324
vishvas.prajapati <vishvas.prajapati@volansys.com>
2023-01-03 20:51:19 +0530

    ui cognito

---
+++ services/Auth/config.ts
@@ -0,0 +1,7 @@
+export const config = {
+  REGION: "ap-south-1",
+  USER_POOL_ID: "ap-south-1_pjXq5woZF",
+  APP_CLIENT_ID: "2ledbcb0vc4089gvlmkukcvd3s",
+  TEST_USER_NAME: 'harshankpatel',
+  TEST_USER_PASSWORD:"Hansu#3597"
+};
